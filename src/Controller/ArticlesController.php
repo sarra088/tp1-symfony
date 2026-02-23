8b7a383793231c32958b785000a6cef7c5eccd5b
sarra088 <sarramlika8@gmail.com>
2026-02-23 21:15:59 +0100

    feat: Entité Article avec Doctrine, migration et CRUD basique

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -1,14 +1,34 @@
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
 
-#[Route('/articles', name: 'app_articles')]
-public function index(): Response
+// ... dans la classe ArticlesController
+
+#[Route('/articles/nouveau', name: 'app_article_nouveau')]
+public function nouveau(EntityManagerInterface $em): Response
 {
-    $articles = [
-        ['titre' => 'Introduction à Symfony',    'auteur' => 'Alice',  'publie' => true],
-        ['titre' => 'Les bases de Twig',          'auteur' => 'Bob',    'publie' => true],
-        ['titre' => 'Doctrine ORM en pratique',   'auteur' => 'Claire', 'publie' => false],
-        ['titre' => 'Sécurité avec Symfony',      'auteur' => 'David',  'publie' => true],
-        ['titre' => 'API Platform (brouillon)',   'auteur' => 'Eve',    'publie' => false],
-    ];
+    $article = new Article();
+    $article->setTitre('Mon premier article');
+    $article->setContenu('Ceci est le contenu de mon premier article créé avec Doctrine.');
+    $article->setAuteur('Étudiant');
+    $article->setDateCreation(new \DateTime());
+    $article->setPublie(true);
+
+    $em->persist($article);
+    $em->flush();
+
+    return new Response("Article créé avec l'id : " . $article->getId());
+}
+#[Route('/articles/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
+public function detail(Article $article): Response
+{
+    return $this->render('articles/detail.html.twig', [
+        'article' => $article,
+    ]);
+}
+#[Route('/articles', name: 'app_articles')]
+public function index(ArticleRepository $articleRepository): Response
+{
+    $articles = $articleRepository->findAll();
 
     return $this->render('articles/index.html.twig', [
         'articles' => $articles,
